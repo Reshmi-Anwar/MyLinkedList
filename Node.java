80728c6b5c60265d1cb19df6f68d55d0f4d98e2e
Reshmi-Anwar <ranwar10@stuy.edu>
2021-01-06 12:05:28 -0500

    created constructor

--- Node.java
+++ Node.java
@@ -1,5 +1,7 @@
 public class Node{
- public Node(String value){/*create a constructor*/}
+ public Node(String value){
+   data = value;
+ }
  private String data;
  private Node next,prev;
  //write get/set methods for all three instance variables.
