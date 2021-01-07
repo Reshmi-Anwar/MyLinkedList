c9d4f3bf57395f6949448d7c0377cb3eea777dcd
Reshmi-Anwar <ranwar10@stuy.edu>
2021-01-06 19:27:31 -0500

    helper

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -4,12 +4,21 @@ public class MyLinkedList{
  public MyLinkedList(){
    size = 0;
  }
+ private Node getNode(int index){
+   int i =0;
+   Node current = start;
+   while(i < index){
+     current = current.getNext();
+     i++;
+   }
+   return current;
+ }
  public int size(){
    return size;
  }
  /*
  public boolean add(String value);
- public boolean add(int index, String value);
+ public void add(int index, String value);
  public String get(int index);
  public String set(int index, String value);
  public String toString();
