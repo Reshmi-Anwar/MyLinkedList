64dabb90ccf5b7b6a5bb79e237a65e17b84f29de
Reshmi-Anwar <ranwar10@stuy.edu>
2021-01-06 20:05:07 -0500

    add

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -4,8 +4,9 @@ public class MyLinkedList{
  public MyLinkedList(){
    size = 0;
  }
+ //private helper
  private Node getNode(int index){
-   int i =0;
+   int i = 0;
    Node current = start;
    while(i < index){
      current = current.getNext();
@@ -16,9 +17,48 @@ public class MyLinkedList{
  public int size(){
    return size;
  }
+
+ public boolean add(String value){
+   if (size == 0){
+     Node n = new Node(value);
+     start = n;
+     end = n;
+   }
+   else{
+     Node n = new Node(value);
+     n.setPrev(end);
+     end.setNext(n);
+     end = n;
+   }
+   size++;
+   return true;
+ }
+
+ public void add(int index, String value){
+   if (index > size){
+     throw new IndexOutOfBoundsException("Out of Bounds");
+   }
+   if(index == 0){
+     Node n = start;//creates node
+     start = new Node(value);//assigns it to value
+     start.setNext(n);
+     n.setPrev(start);
+   }
+   if(index == size()){
+     add(value);
+     return;
+   }
+   else{
+     Node current = getNode(index);
+     Node n = new Node(value);
+     n.setNext(current);
+     n.setPrev(current.getPrev());
+     current.getPrev().setNext(n);
+     current.setPrev(n);
+   }
+   size++;
+ }
  /*
- public boolean add(String value);
- public void add(int index, String value);
  public String get(int index);
  public String set(int index, String value);
  public String toString();
